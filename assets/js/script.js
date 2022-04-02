f981c472cceb294c0cd0b6c891c1a90053499fe9
0ffpudding <jennaanderson00@gmail.com>
2022-04-01 20:01:25 -0500

    added event creation  input form to HTML, added pseudocode outline to script

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// CRITERIA
+// current day displays at the top of calendar
+// standard business hours time blocks
+// blocks are color-coded for past, present, and future
+// can enter events on time blocks
+// can save events in localStorage and give alert
+// saved events persist
+
+// PSEUDOCODE
+// create an array for dates
+// if/then statement to change days based on selected date
+// display input in innerHtml
+// set and get data from localStorage
+// window.alert for saved data
+
+// add saved event types from localStorage to event type dropdown
+// add event to the selected timeblock based on time selected in event creation input form
+// link selected icons from event types 
+// display icons based on event type input
+// if repeating checkbox is selected, show frequency dropdown in innerHtml
+// repeat event based on selected frequency
