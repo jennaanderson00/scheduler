508ed62426b72f7bf9095f553a94e2efeb81519c
0ffpudding <jennaanderson00@gmail.com>
2022-04-03 16:21:50 -0500

    added color classes to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,30 +6,18 @@
 // can save events in localStorage and give alert
 // saved events persist
 
-// PSEUDOCODE
-// create an array for time blocks
-// display input in innerHtml
-// set and get data from localStorage
-// window.alert for saved data
-
-// add saved event types from localStorage to event type dropdown
-// add event to the selected timeblock based on time selected in event creation input form
-// link selected icons from event types 
-// display icons based on event type input
-// if repeating checkbox is selected, show frequency dropdown in innerHtml
-// repeat event based on selected frequency
-
 // Array of time block objects 
 // e.g. [{startTime: 9 am, content: "placeholder", color: "grey"}, ...]
 var timeBlocks = [];
 
 $(function() {
     // placing retrieve here causes currHour to run twice
-    $('#event-creation').css("display", "none");
     // Format todays date now
     var now = moment().format( "MM-DD-YYYY");
     // Use innerText or InnerHTML to put date inside element
     $('#currentDay').html(now);
+
+    var timeBlockElement = $('<div></div>');
 
     // Loop from 9 am to 5 pm and populate timeBlocks with objects 
     const currHour = moment().hour();
@@ -38,20 +26,35 @@ $(function() {
         var timeBlockObj;
         if (i < currHour) {
             timeBlockObj = {icon: '', startTime: i, content: ''};
+            $('#time-blocks').className = 'past';
             // change background color
         } else if (i == currHour) {
             timeBlockObj = {icon: '', startTime: i, content: ''};
+            $('#time-blocks').className = 'present';
         } else {
             timeBlockObj = {icon: '', startTime: i, content: ''};
+            $('#time-blocks').className = 'future';
         }
         timeBlocks.push(timeBlockObj);
     }
     console.log(timeBlocks);
 
-    // Loop through timeBlocks and display / load the content into the dom 
+    //change bg color
+    function changeColor() {
+        if (i < currHour) {
+            timeBlockElement.classname = "past";
+        } else if (i == currHour) {
+            timeBlockElement.classname = "present";
+        } else {
+            timeBlockElement.classname = "future";
+        }
+    }
+    
+
+    // Loop through timeBlocks and display/load the content into the dom 
     for (i = 0; i < timeBlocks.length; i++) {
         var timeBlockIcons = $('<div class="icon"></div>')
-        var timeBlockElement = $('<div></div>');
+        timeBlockElement;
         var timeBlockStartTime = $('<p></p>');
         var timeBlockContent = $('<div></div>');
         var timeBlockInput = $('<input></input>');
@@ -67,6 +70,9 @@ $(function() {
         timeBlockElement.append(timeBlockStartTime, timeBlockContent, timeBlockColor);
         $('#time-blocks').append(timeBlockElement);
     }
+
+    changeColor();
+
     // icons
     var icon9 = $('<i class="fa-solid fa-clock-nine"></i>');
     var icon10 = $('<i class="fa-solid fa-clock-ten"></i>');
@@ -92,13 +98,14 @@ $(function() {
 
     // add icons
     for (i = 9; i < 18; i++) {
-        timeBlockIcons.html(icons.iconObject++)
+        timeBlockIcons.append(icons[i])
     }
 
     // save input on enter keypress event
     $(document).on('keypress', function(e) {
         if(e.which == 13) {
             save();
+            window.alert("Event added!");
         }
     });
 
